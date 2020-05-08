fd451fc4408a42b21e9752dad731f9b2d3510d28
BryantCabrera <bcabrera@gocurrency.com>
2020-05-08 15:34:31 -0700

    Creates 841 solution with descriptive explanation comments.

--- 841_keysAndRooms.js
+++ 841_keysAndRooms.js
@@ -40,3 +40,54 @@
 // 1 <= rooms.length <= 1000
 // 0 <= rooms[i].length <= 1000
 // The number of keys in all rooms combined is at most 3000.
+
+
+/**
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+var canVisitAllRooms = function(rooms) {
+	// Create an array representing the possible list of rooms where each value represents whether or not that room can be indexed
+	const accessibleRooms = new Array(rooms.length).fill(false);
+	// The first room is always accessible
+	accessibleRooms[0] = true;
+	
+	// Initialize the currentRoom to investigate
+	let currentRoom = 0;
+	// Create a queue of rooms we can visit (which is dictated by the keys we get from the currentRoom we are visiting)
+	let queue = [...rooms[0]];
+	
+	// While there are still rooms to investigate, perform this function
+	while (queue.length > 0) {
+			// Update current room
+			currentRoom = queue.shift();
+			
+			// Update the array to show that this room can be accessed
+			accessibleRooms[currentRoom] = true;
+			
+			// Iterate through that room's keys
+			// I chose to decrement so that we can remove keys from the queue without affecting the loop
+			for (let i = rooms[currentRoom].length - 1; i >= 0 ; i--) {
+					// Add the key to the queue, which signifies a room we can investigate
+					queue.push(rooms[currentRoom][i]);
+					// Remove that key from the currentRoom
+					rooms[currentRoom].pop();
+			}
+	}
+	
+	// If even one room isn't accessible, return false
+	return !accessibleRooms.includes(false);
+};
+
+
+// Attempt #1: Incorrect Logic
+//     for (let i = 0; i < rooms.length; i++) {
+//         for (let j = 0; j < rooms[i].length; j++) {
+//             if (i !== rooms[i][j]) {
+//                 keysArr[rooms[i][j]] = true;
+//                 console.log('keysArr: ', keysArr);
+//             }
+//         }
+//     }
+	
+//     return !keysArr.includes(false);
