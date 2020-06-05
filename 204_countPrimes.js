70ad17be786e667db0219bc34078bbfca7de5011
BryantCabrera <cabrera.bryant@gmail.com>
2020-06-05 14:34:38 -0700

    Adds Problem 204 attempt #1.

--- 204_countPrimes.js
+++ 204_countPrimes.js
@@ -8,3 +8,24 @@
 // Input: 10
 // Output: 4
 // Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
+
+
+// Attempt #1
+// /**
+//  * @param {number} n
+//  * @return {number}
+//  */
+// var countPrimes = function(n) {
+// 	const isPrime = num => {
+// 		for (let i = 2; i < num; i++)
+// 			if (num % i === 0) return false;
+// 		return num > 1;
+// 	}
+	
+// 	let count = 0;
+// 	for (let i = 0; i < n; i++) {
+// 			if (isPrime(i)) count++;
+// 	}
+	
+// 	return count;
+// };
