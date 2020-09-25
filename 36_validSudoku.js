eeb717d8ea6d85394d5dc907ec610dd94665e72c
BryantCabrera <cabrera.bryant@gmail.com>
2020-09-25 14:55:54 -0700

    Adds problem 62 and initial solutions.

--- 36_validSudoku.js
+++ 36_validSudoku.js
@@ -49,3 +49,66 @@
 // Only the filled cells need to be validated according to the mentioned rules.
 // The given board contain only digits 1-9 and the character '.'.
 // The given board size is always 9x9.
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+	const isValid = (dataSetArr) => {
+			const hashMap = {};
+			
+			for (let i = 0; i < dataSetArr.length; i++) {
+					if (dataSetArr[i] !== '.' && hashMap[dataSetArr[i]]) return false;
+					
+					hashMap[dataSetArr[i]] = 1;
+			}
+			
+			return true;
+	};
+	
+	// Check rows.
+	for (let j = 0; j < board.length; j++) {
+			if (!isValid(board[j])) return false;    
+	}
+	
+	// Check columns.
+	for (let k = 0; k < board[0].length; k++) {
+			const column = board.reduce( (acc, row) => {
+					acc.push(row[k]);
+					return acc;
+			}, []);
+			
+			console.log(`column ${k}: ${column}`);
+			
+			if (!isValid(column)) return false;
+	}
+	
+	// Check squares.
+	const squares = [];
+	const rowMin = 0;
+	const colMin = 0;
+	
+	while (squares.length < board.length) {
+			const square = [];
+			for (let rowIndex = rowMin; rowIndex < rowMin + 3; rowIndex++) {
+					for (let colIndex = colMin; colIndex < colMin + 3; colIndex++) {
+							square.push(board[rowIndex][colIndex]);
+					}       
+			}
+			
+			squares.push(square);
+	}
+	
+	// for (let rowIndex = 0; rowIndex < 3; rowIndex++) {
+	//     for (let colIndex = 0; colIndex < 3; colIndex++) {
+	//         squares.push(board[rowIndex][colIndex]);
+	//     }       
+	// }
+
+	console.log(`squares: ${squares}`);
+	
+	if (!isValid(squares)) return false;
+	
+	return true;
+};
---
+++ 62_uniquePaths.js
@@ -0,0 +1,110 @@
+// 62. Unique Paths
+// https://leetcode.com/problems/unique-paths/
+
+// 62. Unique Paths
+// Medium
+
+// 3817
+
+// 223
+
+// Add to List
+
+// Share
+// A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+// The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+// How many possible unique paths are there?
+
+ 
+
+// Example 1:
+
+
+// Input: m = 3, n = 7
+// Output: 28
+// Example 2:
+
+// Input: m = 3, n = 2
+// Output: 3
+// Explanation:
+// From the top-left corner, there are a total of 3 ways to reach the bottom-right corner:
+// 1. Right -> Down -> Down
+// 2. Down -> Down -> Right
+// 3. Down -> Right -> Down
+// Example 3:
+
+// Input: m = 7, n = 3
+// Output: 28
+// Example 4:
+
+// Input: m = 3, n = 3
+// Output: 6
+ 
+
+// Constraints:
+
+// 1 <= m, n <= 100
+// It's guaranteed that the answer will be less than or equal to 2 * 109.
+
+
+// Unmemoized Recursive Solution
+// /**
+//  * @param {number} m
+//  * @param {number} n
+//  * @return {number}
+//  */
+// var uniquePaths = function(m, n) {
+// 	const findPaths = (currentPosition) => {
+// 		let right = 0,
+// 			down = 0;
+// 		if (currentPosition[0] == n && currentPosition[1] == m) {
+// 			return 1;
+// 		} else {
+// 			if (currentPosition[0] !== n) {
+// 				right = findPaths([currentPosition[0] + 1, currentPosition[1]]);
+// 			}
+			
+// 			if (currentPosition[1] !== m) {
+// 				down = findPaths([currentPosition[0], currentPosition[1] + 1]);
+// 			}
+			
+// 			console.log(`currentPosition: ${currentPosition}, right: ${right}, down: ${down}`);
+// 			return right + down;
+// 		}
+// 	}
+	
+// 	return findPaths([1, 1]);
+// };
+
+
+// 1st Attempt
+// /**
+//  * @param {number} m
+//  * @param {number} n
+//  * @return {number}
+//  */
+// var uniquePaths = function(m, n) {
+// 	let pathCount = 0;
+	
+// 	// Perform DFS.
+// 	const currentPosition = [1,1];
+// 	const stack = [currentPosition];
+	
+// 	while (stack.length) {
+// 			current = stack.pop();
+// 			console.log(current, '@@@@@current@@@@@');
+// 			if (current[0] === n && current[1] === m) {
+// 					pathCount++;
+// 					continue;
+// 			}
+// 			// The right neighbor.
+// 			if (current[0] !== n) stack.push([current[0] + 1, current[1]]);
+			
+// 			// The down neighbor.
+// 			if (current[1] !== m) stack.push([current[0], current[1] + 1]);
+// 	}
+	
+// 	return pathCount;
+// };
