f352c55943ab0c028db63dd3a5b6e7dfc4107b5c
BryantCabrera <cabrera.bryant@gmail.com>
2020-11-11 21:05:58 -0800

    Creates solution to 344.

--- 344_reverseString.js
+++ 344_reverseString.js
@@ -18,3 +18,24 @@
 
 // Input: ["H","a","n","n","a","h"]
 // Output: ["h","a","n","n","a","H"]
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+	let start = 0;
+	let end = s.length - 1;
+	let temp;
+	
+	while (start < end) {
+			temp = s[start];
+			s[start] = s[end];
+			s[end] = temp;
+			
+			start++;
+			end--;
+	}
+	
+	return s;
+};
