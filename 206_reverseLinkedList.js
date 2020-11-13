db86ad8ae9d767f50c62adb416ccc8c2383bcf0d
BryantCabrera <cabrera.bryant@gmail.com>
2020-11-13 03:14:28 -0800

    Adds problem 206.

---
+++ 206_reverseLinkedList.js
@@ -0,0 +1,23 @@
+// Reverse Linked List
+
+// https://leetcode.com/problems/reverse-linked-list/
+
+// 206. Reverse Linked List
+// Easy
+
+// 5603
+
+// 106
+
+// Add to List
+
+// Share
+// Reverse a singly linked list.
+
+// Example:
+
+// Input: 1->2->3->4->5->NULL
+// Output: 5->4->3->2->1->NULL
+// Follow up:
+
+// A linked list can be reversed either iteratively or recursively. Could you implement both?
