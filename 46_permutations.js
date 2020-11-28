c6c9257abbd818ff00c54e09de86be809d9c698c
BryantCabrera <cabrera.bryant@gmail.com>
2020-11-28 00:05:18 -0800

    Creates solution to 46.

--- 46_permutations.js
+++ 46_permutations.js
@@ -36,37 +36,65 @@
 // -10 <= nums[i] <= 10
 // All the integers of nums are unique.
 
-
-// First Attempt
 /**
  * @param {number[]} nums
  * @return {number[][]}
  */
 var permute = function(nums) {
-	const permutations = [nums];
+	const permutations = [];
 	
-	// Edge Case
-	if (nums.length === 0 || nums.length === 1) return permutations;
+	// Recursion Termintation Conditions.
+	if (nums.length === 0) {
+			return [[]];
+	} else if (nums.length === 1) {
+			return [[nums[0]]];
+	}
 	
 	for (let i = 0; i < nums.length; i++) {
 			// Create a new array without the current element.
 			const numsTruncated = nums.filter( (el, index) => index !== i);
-			console.log(numsTruncated);
-			// const numsDeepCopy = JSON.parse(JSON.stringify(nums));
-			// const currentNum = numsDeepCopy.splice(0, 1);
 			
-			for (let j = i + 1; j < nums.length; j++) {
-					// Create a deep copy of numsTruncated for manipulation.
-					const permutation = JSON.parse(JSON.stringify(numsTruncated));
-					
-					// if (j === i) continue;
-					
-					permutation.splice(j, 0, nums[i]);
-					console.log(permutation, '@@@@@permutation@@@@@');
-					
-					permutations.push(permutation);
+			// Recursively permute the rest of the values.
+			const permutatedValues = permute(numsTruncated);
+						
+			for (let j = 0; j < permutatedValues.length; j++) {
+					permutations.push([nums[i], ...permutatedValues[j]]);
 			}
 	}
 	
 	return permutations;
 };
+
+// First Attempt
+// /**
+//  * @param {number[]} nums
+//  * @return {number[][]}
+//  */
+// var permute = function(nums) {
+// 	const permutations = [nums];
+	
+// 	// Edge Case
+// 	if (nums.length === 0 || nums.length === 1) return permutations;
+	
+// 	for (let i = 0; i < nums.length; i++) {
+// 			// Create a new array without the current element.
+// 			const numsTruncated = nums.filter( (el, index) => index !== i);
+// 			console.log(numsTruncated);
+// 			// const numsDeepCopy = JSON.parse(JSON.stringify(nums));
+// 			// const currentNum = numsDeepCopy.splice(0, 1);
+			
+// 			for (let j = i + 1; j < nums.length; j++) {
+// 					// Create a deep copy of numsTruncated for manipulation.
+// 					const permutation = JSON.parse(JSON.stringify(numsTruncated));
+					
+// 					// if (j === i) continue;
+					
+// 					permutation.splice(j, 0, nums[i]);
+// 					console.log(permutation, '@@@@@permutation@@@@@');
+					
+// 					permutations.push(permutation);
+// 			}
+// 	}
+	
+// 	return permutations;
+// };
