137f8c23471d02778172d8b7d400f7a56bdf7f87
BryantCabrera <cabrera.bryant@gmail.com>
2020-11-27 23:54:20 -0800

    Creates first attempt at 46.

--- 46_permutations.js
+++ 46_permutations.js
@@ -35,3 +35,38 @@
 // 1 <= nums.length <= 6
 // -10 <= nums[i] <= 10
 // All the integers of nums are unique.
+
+
+// First Attempt
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+	const permutations = [nums];
+	
+	// Edge Case
+	if (nums.length === 0 || nums.length === 1) return permutations;
+	
+	for (let i = 0; i < nums.length; i++) {
+			// Create a new array without the current element.
+			const numsTruncated = nums.filter( (el, index) => index !== i);
+			console.log(numsTruncated);
+			// const numsDeepCopy = JSON.parse(JSON.stringify(nums));
+			// const currentNum = numsDeepCopy.splice(0, 1);
+			
+			for (let j = i + 1; j < nums.length; j++) {
+					// Create a deep copy of numsTruncated for manipulation.
+					const permutation = JSON.parse(JSON.stringify(numsTruncated));
+					
+					// if (j === i) continue;
+					
+					permutation.splice(j, 0, nums[i]);
+					console.log(permutation, '@@@@@permutation@@@@@');
+					
+					permutations.push(permutation);
+			}
+	}
+	
+	return permutations;
+};
