318da4db16172ba14a3db35294d18e9724c48069
BryantCabrera <cabrera.bryant@gmail.com>
2020-11-28 01:09:29 -0800

    Creates solution to 21.

--- 21_mergeTwoSortedLists.js
+++ 21_mergeTwoSortedLists.js
@@ -43,21 +43,32 @@ var mergeTwoLists = function(l1, l2) {
 	let l1Current = l1;
 	let l2Current = l2;
 	
-	let startingValue;
-	if (l1Current.val < l2Current.val) {
+	let startingValue = null;
+	if (l1Current !== null && l2Current !== null) {
+			if (l1Current.val < l2Current.val) {
+					startingValue = l1Current.val;
+					l1Current = l1Current.next;
+			} else {
+					// If l2's head value is less than OR equal to l1's head value, use l2's head for the starting value.
+					startingValue = l2Current.val;
+					l2Current = l2Current.next;
+			}
+	} else if (l1Current !== null && l2Current === null) {
 			startingValue = l1Current.val;
 			l1Current = l1Current.next;
-	} else {
-			// If l2's head value is less than OR equal to l1's head value, use l2's head for the starting value.
+	} else if (l1Current === null && l2Current !== null) {
 			startingValue = l2Current.val;
 			l2Current = l2Current.next;
+	} else if (l1Current === null && l2Current === null) {
+			// Edge case: if both nodes are null.
+			return null;
 	}
 	
+	
 	const solution = new ListNode(startingValue);
-	console.log(solution, solution.next, '@@@@@solution@@@@@');
 	
 	let solutionCurrent = solution;
-	while (l1Current !== null ?? l2Current !== null) {
+	while (l1Current ?? l2Current) {
 			if (solutionCurrent === null) break;
 			
 			if (l1Current !== null && l2Current !== null) {
@@ -65,17 +76,18 @@ var mergeTwoLists = function(l1, l2) {
 							solutionCurrent.next = new ListNode(l1Current.val);
 							l1Current = l1Current.next;
 					} else {
+							// If l2Current's value is less than or equal to l1Current's value, use l2Current's value for the next node's value.
 							solutionCurrent.next = new ListNode(l2Current.val);
 							l2Current = l2Current.next;
 					}
-					
 			} else if (l1Current !== null && l2Current === null) {
+					// If l2Current is null, use l1Current's value for the next node's value.
 					if (l2Current === null || l1Current.val < l2Current.val) {
 							solutionCurrent.next = new ListNode(l1Current.val);
 							l1Current = l1Current.next;
 					}
 			} else if (l2Current !== null && l1Current === null) {
-					// If l2's head value is less than or equal to l1's head value OR l1Current is null, use l2's head for the next node's value.
+					// If l1Current is null, use l2Current's value for the next node's value.
 					solutionCurrent.next = new ListNode(l2Current.val);
 					l2Current = l2Current.next;
 			}
@@ -83,11 +95,73 @@ var mergeTwoLists = function(l1, l2) {
 			if (solutionCurrent !== null) {
 					solutionCurrent = solutionCurrent.next;
 			}
-			console.log(l1Current, l2Current, solutionCurrent, '@@@@@solutionCurrent@@@@@');
 	}
 	
 	return solution;
 };
+
+// Second Attempt on 11.28.20
+// /**
+//  * Definition for singly-linked list.
+//  * function ListNode(val) {
+//  *     this.val = val;
+//  *     this.next = null;
+//  * }
+//  */
+// /**
+//  * @param {ListNode} l1
+//  * @param {ListNode} l2
+//  * @return {ListNode}
+//  */
+// var mergeTwoLists = function(l1, l2) {
+// 	let l1Current = l1;
+// 	let l2Current = l2;
+	
+// 	let startingValue;
+// 	if (l1Current.val < l2Current.val) {
+// 			startingValue = l1Current.val;
+// 			l1Current = l1Current.next;
+// 	} else {
+// 			// If l2's head value is less than OR equal to l1's head value, use l2's head for the starting value.
+// 			startingValue = l2Current.val;
+// 			l2Current = l2Current.next;
+// 	}
+	
+// 	const solution = new ListNode(startingValue);
+// 	console.log(solution, solution.next, '@@@@@solution@@@@@');
+	
+// 	let solutionCurrent = solution;
+// 	while (l1Current !== null ?? l2Current !== null) {
+// 			if (solutionCurrent === null) break;
+			
+// 			if (l1Current !== null && l2Current !== null) {
+// 					if (l1Current.val < l2Current.val) {
+// 							solutionCurrent.next = new ListNode(l1Current.val);
+// 							l1Current = l1Current.next;
+// 					} else {
+// 							solutionCurrent.next = new ListNode(l2Current.val);
+// 							l2Current = l2Current.next;
+// 					}
+					
+// 			} else if (l1Current !== null && l2Current === null) {
+// 					if (l2Current === null || l1Current.val < l2Current.val) {
+// 							solutionCurrent.next = new ListNode(l1Current.val);
+// 							l1Current = l1Current.next;
+// 					}
+// 			} else if (l2Current !== null && l1Current === null) {
+// 					// If l2's head value is less than or equal to l1's head value OR l1Current is null, use l2's head for the next node's value.
+// 					solutionCurrent.next = new ListNode(l2Current.val);
+// 					l2Current = l2Current.next;
+// 			}
+			
+// 			if (solutionCurrent !== null) {
+// 					solutionCurrent = solutionCurrent.next;
+// 			}
+// 			console.log(l1Current, l2Current, solutionCurrent, '@@@@@solutionCurrent@@@@@');
+// 	}
+	
+// 	return solution;
+// };
 
 // First Attempt from 10.01.2019
 // /**
