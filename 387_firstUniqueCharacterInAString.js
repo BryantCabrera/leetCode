9444b5fdfc4f191c1c51e6e99963f73896228731
BryantCabrera <cabrera.bryant@gmail.com>
2020-12-08 21:42:55 -0800

    Creates solution to 387.

--- 387_firstUniqueCharacterInAString.js
+++ 387_firstUniqueCharacterInAString.js
@@ -24,3 +24,24 @@
  
 
 // Note: You may assume the string contains only lowercase English letters.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function(s) {
+	const charMap = {};
+	
+	for (const char of s) {
+			charMap[char] = charMap[char] ? charMap[char] + 1 : 1;
+	}
+	
+	
+	for (let i = 0; i < s.length; i++) {
+			if (charMap[s[i]] === 1) {
+					return i;
+			}
+	}
+					
+	return -1;
+};
