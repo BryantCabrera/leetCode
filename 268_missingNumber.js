abc74cffd76c706abb28e9e078862216c66df429
BryantCabrera <cabrera.bryant@gmail.com>
2020-12-08 22:19:04 -0800

    Creates solution to 268.

--- 268_missingNumber.js
+++ 268_missingNumber.js
@@ -52,8 +52,13 @@
  * @return {number}
  */
 var missingNumber = function(nums) {
-	nums.sort();
+	nums.sort(function (a, b) {
+			return a - b;
+	}); 
+
 	for (let i = 0; i < nums.length; i++) {
 			if (nums[i] !== i) return i;
 	}
+	
+	return nums.length;
 };
