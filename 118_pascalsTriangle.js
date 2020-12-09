41763cc0310e7b0aa0e04b8b27142c876e2594f5
BryantCabrera <cabrera.bryant@gmail.com>
2020-12-08 21:29:08 -0800

    Creates solution to 118.

--- 118_pascalsTriangle.js
+++ 118_pascalsTriangle.js
@@ -28,3 +28,29 @@
 //   [1,3,3,1],
 //  [1,4,6,4,1]
 // ]
+
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+	const solution = [];
+	
+	let counter = 1;
+	while (counter <= numRows) {
+			const row = [];
+			for (let i = 0; i < counter; i++) {
+					if (i === 0 || i === counter - 1) {
+							row.push(1);
+					} else {
+							row.push(solution[counter - 2][i - 1] + solution[counter - 2][i])
+					}
+			}
+			
+			solution.push(row);
+
+			counter++;
+	}
+	
+	return solution;
+};
