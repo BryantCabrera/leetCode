f16044c5638ad6f58ca122ab69c760d3de85aa44
BryantCabrera <cabrera.bryant@gmail.com>
2020-12-08 21:30:41 -0800

    Adds problem 387.

---
+++ 387_firstUniqueCharacterInAString.js
@@ -0,0 +1,26 @@
+// First Unique Character In A String
+
+// https://leetcode.com/problems/first-unique-character-in-a-string/
+
+// 387. First Unique Character in a String
+// Easy
+
+// 2426
+
+// 126
+
+// Add to List
+
+// Share
+// Given a string, find the first non-repeating character in it and return its index. If it doesn't exist, return -1.
+
+// Examples:
+
+// s = "leetcode"
+// return 0.
+
+// s = "loveleetcode"
+// return 2.
+ 
+
+// Note: You may assume the string contains only lowercase English letters.
