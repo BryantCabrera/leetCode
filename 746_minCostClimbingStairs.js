228d600efc98a6860a3e006bd8967cef5959d410
BryantCabrera <cabrera.bryant@gmail.com>
2020-12-26 23:02:42 -0800

    Adds problem 198.

--- 746_minCostClimbingStairs.js
+++ 746_minCostClimbingStairs.js
@@ -27,3 +27,34 @@
 // Note:
 // cost will have a length in the range [2, 1000].
 // Every cost[i] will be an integer in the range [0, 999].
+
+
+// First Attempt
+// /**
+//  * @param {number[]} cost
+//  * @return {number}
+//  */
+// var minCostClimbingStairs = function(cost) {
+// 	// Initializes a list of the minimum cost up to and including that step (index).    
+// 	const costList = new Array(cost.length);
+	
+// 	// Edge Cases: If the cost array is null or has no steps, there is no cost.
+// 	if (cost === null || cost.length === 0) return 0;
+	
+// 	// Edge Case: If cost array's length is 1, return the 1st and only cost (the 1st element).
+// 	if (cost.length === 1) return cost[0];
+	
+// 	// Assign the 1st 2 elements of costList.
+// 	costList[0] = cost[0];
+// 	costList[1] = Math.min(cost[0] + cost[1], cost[1]);
+	
+// 	// Loop through the rest of the steps/costs.
+// 	for (let i = 2; i < cost.length; i++) {
+// 			// The min cost for this step is the min of (getting to this step from 1 steps back) and (getting to this step from 2 steps back).
+// 			costList[i] = Math.min(costList[i - 1], costList[i - 2] + cost[i]);
+// 	}
+			
+// 	console.log(costList, '@@@@@costList@@@@@');
+// 	// The last element's index in the costList will correspond to the last step. The element itself represents the minimum cost to get to that step.
+// 	return costList[costList.length - 1];
+// };
