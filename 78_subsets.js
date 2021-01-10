c3fbce6eaf6aa3eeae5fa9d8f5099b1f81cc658c
BryantCabrera <cabrera.bryant@gmail.com>
2021-01-09 20:12:57 -0800

    Creates solution to 78.

--- 78_subsets.js
+++ 78_subsets.js
@@ -33,3 +33,26 @@
 // 1 <= nums.length <= 10
 // -10 <= nums[i] <= 10
 // All the numbers of nums are unique.
+
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+	const solution = [[]];
+	
+	// To create a new subset, we can loop through the integers in the nums array and create a copy of each subset array currently in solution that includes the current integer.
+	for (let i = 0; i < nums.length; i++) {
+			// Snapshot of current length of solution.
+			let counter = solution.length;
+			
+			for (let j = 0; j < counter; j++)  {
+					const subset = [...solution[j], nums[i]];
+					
+					solution.push(subset);
+			}
+	}
+	
+	return solution;
+};
