c71fe48e2a102c378b48298506f5df1b0fcdcacc
BryantCabrera <cabrera.bryant@gmail.com>
2021-01-10 17:45:43 -0800

    Creates solution to 938.

--- 938_rangeSumOfBST.js
+++ 938_rangeSumOfBST.js
@@ -34,3 +34,39 @@
 // 1 <= Node.val <= 105
 // 1 <= low <= high <= 105
 // All Node.val are unique.
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function(root, low, high) {
+	let sum = 0;
+	
+	const traverseTree = (node) => {
+			if (node === null) return 0;
+			
+			if (node.val >= low && node.val <= high) {
+					sum += node.val;
+			}
+			
+			const left = traverseTree(node.left);
+			const right = traverseTree(node.right);
+
+			return node.val;
+	};
+	
+	traverseTree(root);
+	
+	return sum;
+};
