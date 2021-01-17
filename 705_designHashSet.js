8605af6e580c05d6c40d45fb9864cef76a636739
BryantCabrera <cabrera.bryant@gmail.com>
2021-01-17 14:38:37 -0800

    Creates solution to 705.

--- 705_designHashSet.js
+++ 705_designHashSet.js
@@ -37,3 +37,44 @@
 // All values will be in the range of [0, 1000000].
 // The number of operations will be in the range of [1, 10000].
 // Please do not use the built-in HashSet library.
+
+
+/**
+ * Initialize your data structure here.
+ */
+var MyHashSet = function() {
+	this.hashSet = [];
+};
+
+/** 
+* @param {number} key
+* @return {void}
+*/
+MyHashSet.prototype.add = function(key) {
+	this.hashSet[key] = 1;
+};
+
+/** 
+* @param {number} key
+* @return {void}
+*/
+MyHashSet.prototype.remove = function(key) {
+	this.hashSet[key] = 0;
+};
+
+/**
+* Returns true if this set contains the specified element 
+* @param {number} key
+* @return {boolean}
+*/
+MyHashSet.prototype.contains = function(key) {
+	return this.hashSet[key] === 1;
+};
+
+/** 
+* Your MyHashSet object will be instantiated and called as such:
+* var obj = new MyHashSet()
+* obj.add(key)
+* obj.remove(key)
+* var param_3 = obj.contains(key)
+*/
