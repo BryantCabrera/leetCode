8ef4b4d23a7f11e4f5cc6ac419bc796fe25516e3
BryantCabrera <cabrera.bryant@gmail.com>
2021-01-17 14:27:35 -0800

    Creates solution to 706.

--- 706_designnHashMap.js
+++ 706_designnHashMap.js
@@ -37,3 +37,47 @@
 // All keys and values will be in the range of [0, 1000000].
 // The number of operations will be in the range of [1, 10000].
 // Please do not use the built-in HashMap library.
+
+
+/**
+ * Initialize your data structure here.
+ */
+var MyHashMap = function() {
+	this.hashMap = {};
+};
+
+/**
+* value will always be non-negative. 
+* @param {number} key 
+* @param {number} value
+* @return {void}
+*/
+MyHashMap.prototype.put = function(key, value) {
+	this.hashMap[key] = value;
+};
+
+/**
+* Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key 
+* @param {number} key
+* @return {number}
+*/
+MyHashMap.prototype.get = function(key) {
+	return this.hashMap[key] >= 0 ? this.hashMap[key] : -1;
+};
+
+/**
+* Removes the mapping of the specified value key if this map contains a mapping for the key 
+* @param {number} key
+* @return {void}
+*/
+MyHashMap.prototype.remove = function(key) {
+	delete this.hashMap[key];
+};
+
+/** 
+* Your MyHashMap object will be instantiated and called as such:
+* var obj = new MyHashMap()
+* obj.put(key,value)
+* var param_2 = obj.get(key)
+* obj.remove(key)
+*/
