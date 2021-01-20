2063c965cc3909cb1d39b2aee968ef02702c51a4
BryantCabrera <cabrera.bryant@gmail.com>
2021-01-19 20:39:33 -0800

    Creates solution to 695.

--- 695_maxAreaOfIsland.js
+++ 695_maxAreaOfIsland.js
@@ -32,3 +32,56 @@
 // [[0,0,0,0,0,0,0,0]]
 // Given the above grid, return 0.
 // Note: The length of each dimension in the given grid does not exceed 50.
+
+
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var maxAreaOfIsland = function(grid) {
+	// Edge Case: If the grid is null or has no elements, there are no islands.
+	if (grid === null || grid.length === 0) return 0;
+	
+	let maxArea = 0;
+	
+	// Loop through each node in the grid to see if it is an island (a '1').
+	for (let i = 0; i < grid.length; i++) {
+			for (let j = 0; j < grid[i].length; j++) {
+					if (grid[i][j] === 1) {
+							// Determine its area.
+							// maxArea is the max of the current max and this island's area.
+							maxArea = Math.max(maxArea, getIslandArea(grid, i, j));
+					}
+			}
+	}
+	
+	return maxArea;
+};
+
+const getIslandArea = (matrix, row, col) => {
+	// Edge Cases:
+	// If the row or col are not in the bounds of the grid, this is not a valid node to check. 
+	// If the node is 0, it does not add to the area/does not need to be marked.
+	if (row < 0 || row > matrix.length - 1 || col < 0 || col > matrix[row].length - 1 || matrix[row][col] === 0) {
+			return 0;
+	}
+	
+	let currentArea = 1;
+	// Mark the current node as counted by reassigning its value to '0'.
+	matrix[row][col] = 0;
+	
+	// Find 4-directionally connected land nodes.
+	// Up
+	currentArea += getIslandArea(matrix, row - 1, col);
+	
+	// Down
+	currentArea += getIslandArea(matrix, row + 1, col);
+	
+	// Left
+	currentArea += getIslandArea(matrix, row, col - 1);
+	
+	// Right
+	currentArea += getIslandArea(matrix, row, col + 1);
+	
+	return currentArea;
+};
