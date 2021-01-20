ca51c0569f2f506a7bd2945b47eae732b4976be6
BryantCabrera <cabrera.bryant@gmail.com>
2021-01-19 18:54:09 -0800

    Creates solution to 234.

--- 234_palindromeLinkedList.js
+++ 234_palindromeLinkedList.js
@@ -24,3 +24,46 @@
 // Output: true
 // Follow up:
 // Could you do it in O(n) time and O(1) space?
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+	// Build an array of the values in the Linked List.
+	const values = [];
+	
+	// Traverse the entire linked list to get the values.
+	let current = head;
+	
+	while (current !== null) {
+			values.push(current.val);
+			current = current.next;
+	}
+
+	// Loop through the values with 1 pointer at the start and another pointer at the end.
+	// At each point, if the 2 numbers do not equal each other, the list is not a Palindrome.
+	let start = 0;
+	let end = values.length - 1;
+	
+	// We can terminate the loop when start >= end because:
+	// If start > end, we have already checked those values.
+	// If start === end, they are on the same element in the array and will thus be equal.
+	while (start < end) {
+			if (values[start] !== values[end]) return false;
+			
+			// Move our start and end pointers.
+			start++;
+			end--;
+	}
+	
+	return true;
+};
