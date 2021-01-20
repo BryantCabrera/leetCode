9bb589b5b74d1f7b8f5a7dae90fa45e8530432d1
BryantCabrera <cabrera.bryant@gmail.com>
2021-01-19 21:48:34 -0800

    Creates RegEx solution to 58.

--- 58_lengthOfLastWord.js
+++ 58_lengthOfLastWord.js
@@ -32,3 +32,34 @@
 
 // 1 <= s.length <= 104
 // s consists of only English letters and spaces ' '.
+
+
+// RegEx Solution
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+	// Edge Case: If there are no words in the string, return 0.
+	const wordRegEx = new RegExp(/\w/, 'g');
+	if (!wordRegEx.test(s)) return 0;
+	
+	// Edge Case: If there is only 1 word, return the length of that word.
+	const spaceRegEx = new RegExp(/\s/, 'g');
+	if (!spaceRegEx.test(s)) return s.length;
+	
+	// Edge Case: If the string ends with a space.
+	if (s[s.length - 1] === ' ') {
+			const trailingSpaceRegEx = new RegExp(/((\w+)(\s+))+/, 'g');
+			const matchWordBeforeSpace = trailingSpaceRegEx.exec(s);
+			
+			return matchWordBeforeSpace[2].length;
+	}
+	
+	// RegEx that matches a space before a word.
+	const regEx = new RegExp(/((\s+)(\w+))+/, 'g');
+	const match = regEx.exec(s);
+	
+	// Return the last capture group's length.
+	return match[match.length - 1].length;
+};
