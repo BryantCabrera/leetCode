6bce45bcce3084f0a73abaa673a5e7caea23557e
BryantCabrera <cabrera.bryant@gmail.com>
2021-01-20 22:48:40 -0800

    Creates solution to 110.

--- 110_balancedBinaryTree.js
+++ 110_balancedBinaryTree.js
@@ -40,3 +40,43 @@
 
 // The number of nodes in the tree is in the range [0, 5000].
 // -104 <= Node.val <= 104
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function(root) {
+	// Edge case: If root is null, it is balanced.
+	if (root === null) return true;
+	
+	// If our getHeight recursive call returns a -1, it is unbalanced.
+	return getHeight(root) !== -1;
+};
+
+const getHeight = (node) => {
+	// A null node does not contribute to the height of the tree.
+	if (node === null) return 0;
+	
+	// Recursively determine the heights of the node's children.
+	const left = getHeight(node.left);
+	const right = getHeight(node.right);
+	
+	// If any child of the node is unbalanced, the rest of the tree will be unbalanced.
+	// If the left branch is greater than the right branch by more than 1 level, the tree is unbalanced.
+	if (left === -1 || right === -1 || Math.abs(left - right) > 1) {
+			return -1;
+	}
+	
+	// We use the max of left or right because the child with the most nodes will contribute to the overall height of that branch.
+	// We add 1 to the max to account for the currentNode contributing to the height of the tree.
+	return Math.max(left, right) + 1;
+};
