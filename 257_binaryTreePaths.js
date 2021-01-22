98ac242e2620f070808a8a464df1032bb6a40b47
BryantCabrera <cabrera.bryant@gmail.com>
2021-01-21 20:34:04 -0800

    Creates solution to 257.

--- 257_binaryTreePaths.js
+++ 257_binaryTreePaths.js
@@ -29,3 +29,44 @@
 // Output: ["1->2->5", "1->3"]
 
 // Explanation: All root-to-leaf paths are: 1->2->5, 1->3
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {  
+	const solution = [];
+	
+	// Edge Case: If the root is null, there are no leaves.
+	if (root === null) return solution;
+	
+	// Recursively traverse the tree (Depth First Search) and build each subpath.
+	getPath(root, '', solution);
+
+	return solution;
+};
+
+const getPath = (node, str, arr) => {
+	// Once we have reached a null node, we don't add that to the subpath string.
+	if (node === null) return null;
+	
+	// Build the subpath string.
+	str += `${node.val}->`;
+	
+	const left = getPath(node.left, str, arr);
+	const right = getPath(node.right, str, arr);
+	
+	// Only when both children are null (a.k.a. the node has no children) do we complete the subpath string and add it to the array.
+	if (left === null && right === null) {
+			arr.push(str.substring(0, str.length - 2));
+	}
+};
