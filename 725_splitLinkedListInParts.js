e8d538d532e50b16698d4866b0f32c8edd4e7ece
BryantCabrera <cabrera.bryant@gmail.com>
2021-02-20 22:19:21 -0800

    Adds problem 941.

--- 725_splitLinkedListInParts.js
+++ 725_splitLinkedListInParts.js
@@ -41,3 +41,75 @@
 // The length of root will be in the range [0, 1000].
 // Each value of a node in the input will be an integer in the range [0, 999].
 // k will be an integer in the range [1, 50].
+
+
+
+// Solution for variation of problem.
+// Created 1 -> 4 -> 7 instead of 1 -> 2 -> 3
+// /**
+//  * Definition for singly-linked list.
+//  * function ListNode(val, next) {
+//  *     this.val = (val===undefined ? 0 : val)
+//  *     this.next = (next===undefined ? null : next)
+//  * }
+//  */
+// /**
+//  * @param {ListNode} root
+//  * @param {number} k
+//  * @return {ListNode[]}
+//  */
+// var splitListToParts = function(root, k) {
+// 	// Edge Case: If root is null, or its length is 0, return an empty solution set.;
+// 	if (root === null) return new Array(k).fill(null);
+	
+// 	// Edge Case:If k is 1, we just return the LinkedList as is.
+// 	if (k === 1) return [root];
+	
+// 	const queue = [];
+	
+// 	let current = root;
+	
+// 	// Create the queue of nodes to process/split into new LinkedLists.
+// 	while (current !== null) {
+// 			queue.push(current); 
+// 			current = current.next;
+// 	}
+	
+// 	console.log(queue, '@@@@@queue@@@@@');
+	
+// 	// Initialize the solution to an empty array filled with nulls so that if there aren't enough nodes to fill all of the indices, empty nodes will already have the value we want to return.
+// 	const solution = new Array(k);
+// 	solution.fill(null);
+// 	console.log(solution, '@@@@@solution array initialized@@@@@');
+	
+// 	// Process all of the nodes in the queue.
+// 	for (let i = 0; i < queue.length; i++) {
+// 			// Determine which LinkedList/index the current node will go to.
+// 			// If there are more nodes than k, we start adding nodes again at the beginning of the solution array.
+// 			const allocatedList = i < k ? i : i % k;
+			
+// 			if (solution[allocatedList]) { 
+// 					// If the current index already has a LinkedList initialized, add the node to the list.
+// 					solution[allocatedList] = addNodeToLinkedList(solution[allocatedList], new ListNode(queue[i].val));
+// 			} else {
+// 					// Else, initialize a new LinkedList with the value of the node from the queue being processed.
+// 					solution[allocatedList] = new ListNode(queue[i].val);
+// 			}
+			
+// 			console.log(i, allocatedList, solution, '@@@@@i, allocatedList, solution@@@@@');
+// 	}
+	
+// 	return solution;
+// };
+
+// const addNodeToLinkedList = (linkedList, node) => {
+// 	let current = linkedList;
+	
+// 	while (current.next !== null) {
+// 			current = current.next;
+// 	}
+	
+// 	current.next = node;
+	
+// 	return linkedList;
+// };
