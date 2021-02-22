203c7fc9cf5dc15dbc1d043dfeec3ddd40d5f8e7
BryantCabrera <cabrera.bryant@gmail.com>
2021-02-21 19:39:47 -0800

    Creates solution to 1314.

--- 1314_matrixBlockSum.js
+++ 1314_matrixBlockSum.js
@@ -31,3 +31,47 @@
 // n == mat[i].length
 // 1 <= m, n, K <= 100
 // 1 <= mat[i][j] <= 100
+
+
+/**
+ * @param {number[][]} mat
+ * @param {number} K
+ * @return {number[][]}
+ */
+var matrixBlockSum = function(mat, K) {
+	// Initialize an array the same size as mat.
+	const solution = new Array(mat.length);
+	
+	// Determine the values for each cell of the solution matrix.
+	for (let i = 0; i < solution.length; i++) {
+			// Initialize the solution matrix's row with an array the same size as a row in mat.
+			solution[i] = (new Array(mat[i].length))
+			for (let j = 0; j < solution[i].length; j++) {
+					// Determine the sum for that cell given the parameters outline in the problem.
+					solution[i][j] = determineSum(mat, i - K, i + K, j - K, j + K);
+			}
+	}
+
+	return solution;
+};
+	
+const determineSum = (matrix, rowMin, rowMax, colMin, colMax) => {
+	let sum = 0;
+	
+	// For each possible row index,
+	for (let i = rowMin; i <= rowMax; i++) {
+			// If the row index falls out of bounds of the matrix, ignore it.
+			if (i < 0 || i > matrix.length - 1) continue;
+			
+			// Pair each possible row value with every possible column index.
+			for (let j = colMin; j <= colMax; j++) {
+					// If the column index falls out of bounds of the matrix, ignore it.
+					if (j < 0 || j > matrix[i].length - 1) continue;
+					
+					// Add the cell's value to the running sum.
+					sum += matrix[i][j];
+			}
+	}
+	
+	return sum;
+};
