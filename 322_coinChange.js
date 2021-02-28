3ca370a3fabe85c452ef52cfd7f4c0710b5d888c
BryantCabrera <cabrera.bryant@gmail.com>
2021-02-27 19:14:54 -0800

    Adds problem 392.

--- 322_coinChange.js
+++ 322_coinChange.js
@@ -46,3 +46,45 @@
 // 1 <= coins.length <= 12
 // 1 <= coins[i] <= 231 - 1
 // 0 <= amount <= 104
+
+
+// First Attempt
+// /**
+//  * @param {number[]} coins
+//  * @param {number} amount
+//  * @return {number}
+//  */
+// var coinChange = function(coins, amount) {
+// 	// Sort the coins so that we can easily traverse from the largest to the smallest, optimizing the least # of coins.
+// 	coins.sort(function (a, b) {
+// 		 return a - b;
+//  });
+// 	console.log(coins, amount, '@@@@@coins, amount@@@@@');
+	
+// 	// Initialize number of coins to 0 so that we can add to eat easier.
+// 	let solution = 0;
+	
+// 	// Loop bakcwards through the array from largest denomination to smallest.
+// 	for (let i = coins.length - 1; i >= 0; i--) {
+// 			// Cache how many times the current denomination can go into the amount.
+// 			const numCoins = Math.floor(amount / coins[i]);
+			
+// 			// If the denomination is not larger than the current amount,
+// 			if (numCoins > 0) {
+// 					// Add that number to total number of coins.
+// 					solution += numCoins;
+					
+// 					// The new amount is the remainder of the amount divided by the denomination.
+// 					amount = amount % coins[i];
+// 			}
+			
+// 			console.log(solution, amount, '@@@@@solution, amount@@@@@');
+// 	}
+	
+// 	// If there is any remainder, this is not a valid set of denominations.
+// 	if (amount) {
+// 			solution = -1;
+// 	}
+	
+// 	return solution;
+// };
