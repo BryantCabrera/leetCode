27d4c8f8e1f8d0b1cde03af2221f1dad9a0424fc
BryantCabrera <cabrera.bryant@gmail.com>
2021-02-27 19:47:05 -0800

    Creates solution to 792.

--- 792_numberOfMatchingSubsequences.js
+++ 792_numberOfMatchingSubsequences.js
@@ -26,3 +26,38 @@
 // The length of S will be in the range of [1, 50000].
 // The length of words will be in the range of [1, 5000].
 // The length of words[i] will be in the range of [1, 50].
+
+
+/**
+ * @param {string} S
+ * @param {string[]} words
+ * @return {number}
+ */
+var numMatchingSubseq = function(S, words) {
+	let solution = 0;
+	
+	// Loop through all of the words in the words array.
+	for (let i = 0; i < words.length; i++) {
+			// If that word is a valid subsequence of S, increment the solution counter.
+			if (isSubsequence(words[i], S)) solution++;
+	}
+			 
+	return solution;
+};
+
+const isSubsequence = function(s, t) {
+	// We will start the search for the 0th indexed character in s among the characters in t.
+	let currentSIndex = 0;
+	
+	// Loop through the characters in t in order.
+	for (let i = 0; i < t.length; i++) {
+			// If we have already found the entirety of s to be a substring of t, break.
+			if (currentSIndex > s.length - 1) break;
+			
+			// If the current character in s is a character t, we will look for the next character in s.
+			if (t[i] === s[currentSIndex]) currentSIndex++;
+	}
+	
+	// If we have incremented past the length of s, that means we have found its entirety in t, and s is thus a substring of t.
+	return currentSIndex === s.length;
+};
