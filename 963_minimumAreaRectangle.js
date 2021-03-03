a4c214171a1c7b1a88a2da414764b387eafd76ef
BryantCabrera <cabrera.bryant@gmail.com>
2021-03-02 20:57:30 -0800

    Creates 1st attempt scaffold for 963.  Adds problem 303.

--- 963_minimumAreaRectangle.js
+++ 963_minimumAreaRectangle.js
@@ -55,3 +55,43 @@
 // 0 <= points[i][1] <= 40000
 // All points are distinct.
 // Answers within 10^-5 of the actual value will be accepted as correct.
+
+
+// First Attempt Scaffold
+// /**
+//  * @param {number[][]} points
+//  * @return {number}
+//  */
+// var minAreaFreeRect = function(points) {
+    
+// };
+
+// const isParallel = ([x1, y1], [x2, y2], [x3, y3], [x4, y4]) => {
+//     // Parallel lines have the same slope.
+    
+//     // Determine the slope of line 1.
+//     const slope1 = (y2 - y1) / (x2 - x1);
+    
+//     // Determine the slope of line 2.
+//     const slope2 = (y4 - y3) / (x4 - x3);
+    
+//     return slope1 === slope2;
+// };
+
+// const isPerpendicular = ([x1, y1], [x2, y2], [x3, y3], [x4, y4]) => {
+//     // Perpendicular lines have negative reciprocal slopes of each other.
+    
+//     // Determine the slope of line 1.
+//     const slope1 = (y2 - y1) / (x2 - x1);
+    
+//     // Determine the slope of line 2.
+//     const slope2 = (y4 - y3) / (x4 - x3);
+    
+//     return slope1 === -(1 / slope2);
+// };
+
+// const findDistance = ([x1, y1], [x2, y2]) => {
+//     // The distance is the length of the hypotenuse where the legs are the differences of the x and y coordinates.
+    
+//     return Math.sqrt((x1 - x2)**2 + (y1 - y2)**2);
+// };
